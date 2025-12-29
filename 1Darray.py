a031837d7a074a4d6e9718c7435a533adf8c23a5
Sujith Kumar <l.sujithkumar154@gmail.com>
2025-12-29 08:36:41 +0530

    Add 1D array Python program

---
+++ 1Darray.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([5, 10, 15, 20])
+print("First element:", arr[0])
+print("Sliced elements (index 1 to 3):", arr[1:3])
+print("Sum:", np.sum(arr))
+print("Maximum:", np.max(arr))
+print("Mean:", np.mean(arr))
